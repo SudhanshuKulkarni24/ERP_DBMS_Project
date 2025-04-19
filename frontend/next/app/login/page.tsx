0c395204f7d7c7603855eeb31ebadd6018484b4c
Sudhanshu Kulkarni <sudhanshu6105@gmail.com>
2025-04-19 16:45:53 +0530

    first

--- frontend/next/app/login/page.tsx
+++ frontend/next/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from 'react'
+import React, { useState, FormEvent } from 'react'
 import {
   Box,
   Button,
@@ -20,12 +20,12 @@ import Link from 'next/link'
 import { signIn } from 'next-auth/react'
 import { FaGoogle } from 'react-icons/fa'
 
-export default function LoginPage() {
+const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const toast = useToast()
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     try {
       const result = await signIn('credentials', {
@@ -72,68 +72,51 @@ export default function LoginPage() {
 
   return (
     <Container maxW="container.sm" py={10}>
-      <VStack spacing={8}>
-        <Box textAlign="center">
-          <Heading as="h1" size="xl" mb={2}>
-            Welcome Back
-          </Heading>
-          <Text color="gray.600">Sign in to your account</Text>
-        </Box>
-
-        <Box as="form" onSubmit={handleSubmit} w="100%" p={8} borderWidth={1} borderRadius="lg">
-          <VStack spacing={4}>
-            <Button
-              w="100%"
-              leftIcon={<FaGoogle />}
-              onClick={handleGoogleSignIn}
-              colorScheme="red"
-              variant="outline"
-            >
-              Sign in with Google
-            </Button>
-
-            <HStack w="100%">
-              <Divider />
-              <Text fontSize="sm" whiteSpace="nowrap" color="gray.500">
-                or continue with email
-              </Text>
-              <Divider />
-            </HStack>
-
-            <FormControl isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter your email"
-              />
-            </FormControl>
-
-            <FormControl isRequired>
-              <FormLabel>Password</FormLabel>
-              <Input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter your password"
-              />
-            </FormControl>
-
-            <Button type="submit" colorScheme="blue" width="100%" mt={4}>
-              Sign In
-            </Button>
-
-            <Text>
-              Don't have an account?{' '}
-              <Link href="/register" passHref>
-                <ChakraLink color="blue.500">Register here</ChakraLink>
-              </Link>
-            </Text>
-          </VStack>
-        </Box>
-      </VStack>
+      <Box p={8} borderWidth={1} borderRadius={8} boxShadow="lg">
+        <VStack spacing={4} align="stretch">
+          <Heading textAlign="center">Sign In</Heading>
+          <Button
+            leftIcon={<FaGoogle />}
+            colorScheme="red"
+            onClick={handleGoogleSignIn}
+          >
+            Sign in with Google
+          </Button>
+          <Divider />
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={4}>
+              <FormControl isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormControl>
+              <Button type="submit" colorScheme="blue" width="full">
+                Sign In
+              </Button>
+            </VStack>
+          </form>
+          <HStack justify="center" mt={4}>
+            <Text>Don't have an account?</Text>
+            <Link href="/register">
+              <Text color="blue.500">Sign up</Text>
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
     </Container>
   )
 }
+
+export default LoginPage
 
